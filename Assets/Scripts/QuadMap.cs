8d7f1a677d851d47c425eef12763ce0641449975
Justin <lord.kuragari@gmail.com>
2014-03-06 16:48:14 -0800

    Create a Quad from scratch
    create a quad and assign vertices, triangles, normals, and uvs.

---
+++ Assets/Scripts/QuadMap.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
+
+
+public class QuadMap : MonoBehaviour {
+
+
+
+	public int verticesCount = 4;
+	// Use this for initialization
+	void Start () 
+	{
+		BuildMesh();
+	}
+	void BuildMesh()
+	{
+		//generage Mesh Data
+		Vector3[] vertices = new Vector3[verticesCount];
+		int[] triangles = new int[6 ]; 
+		Vector3[] normals = new Vector3[verticesCount];
+		Vector2[] uv = new Vector2[verticesCount];
+
+		vertices[0] = new Vector3(0,0,0);
+		vertices[1] = new Vector3(1,0,0);
+		vertices[2] = new Vector3(0,0,-1);
+		vertices[3] = new Vector3(1,0,-1);
+
+		triangles[0] = 0;
+		triangles[1] = 3;
+		triangles[2] = 2;
+
+		triangles[3] = 0;
+		triangles[4] = 1;
+		triangles[5] = 3;
+
+
+
+		normals[0] = Vector3.up;
+		normals[1] = Vector3.up;
+		normals[2] = Vector3.up;
+		normals[3] = Vector3.up;
+		/*
+		 * how it should be 
+		uv[0] = new Vector2(0,0);
+		uv[1] = new Vector3(1,0);
+		uv[2] = new Vector3(0,1);
+		uv[3] = new Vector3(1,1);
+		*/
+		//how it is 
+		//unity considers the bottom left hand coordinate 0,0
+		uv[0] = new Vector2(0,1);
+		uv[1] = new Vector3(1,1);
+		uv[2] = new Vector3(0,0);
+		uv[3] = new Vector3(1,0);
+
+		//create Mesh and populate data;
+		Mesh mesh = new Mesh();
+
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		mesh.normals = normals;
+		mesh.uv = uv;
+
+
+
+
+
+
+
+
+
+
+
+		MeshFilter meshFilter = GetComponent<MeshFilter>();
+		MeshRenderer meshRender = GetComponent<MeshRenderer>();
+		MeshCollider meshCollider = GetComponent<MeshCollider>();
+
+		meshFilter.mesh = mesh;
+	}
+		
+}
---
+++ Assets/Scripts/TileMouseOver.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileMouseOver : MonoBehaviour {
+	public Color highlightColor;
+	Color normalColor;
+	// Use this for initialization
+	void Start () {
+		normalColor = renderer.material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hitInfo;
+		if(collider.Raycast(ray,out hitInfo,100))
+		   {
+			renderer.material.color = highlightColor;
+		}
+		else
+		{
+			renderer.material.color = normalColor;
+		}
+
+
+			
+	}
+}
