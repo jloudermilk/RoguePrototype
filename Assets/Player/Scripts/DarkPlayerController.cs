d80a3410eb9573b1d034e80bc6edd0996d7839bb
Justin <lord.kuragari@gmail.com>
2014-03-19 17:58:29 -0700

    moving sprite working on bomb

---
+++ Assets/Player/Scripts/DarkPlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DarkPlayerController : MonoBehaviour {
+	public Vector2 targetVelocity;
+	private Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	void FixedUpdate () {
+		targetVelocity = new Vector2( Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		rigidbody2D.velocity=targetVelocity * 2.5f;
+		animator.SetFloat("SpeedX",targetVelocity.x);
+		animator.SetFloat("SpeedY",targetVelocity.y);
+	}
+	void LateUpdate(){
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			Application.Quit();
+		}
+	}
+}
