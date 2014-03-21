77a816c74f39328f04fba3a5ed0a8b248d8cb5f0
Justin <lord.kuragari@gmail.com>
2014-03-20 18:03:57 -0700

    Bombs!

---
+++ Assets/Bomb/Scripts/Bomb.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomb : MonoBehaviour {
+	public GameObject Player;
+	public float Timer = 2f;
+	public float Size = 1;
+	Animator animator;
+	float elapsedTime  = 0;
+	public AnimationInfo[] aniInfo;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		elapsedTime +=  Time.deltaTime;
+		if(elapsedTime > Timer && elapsedTime < Timer +.4)
+		{
+			 aniInfo = animator.GetCurrentAnimationClipState(0);
+			aniInfo.
+			animator.Play("Explode");
+		}
+	}
+}
--- Assets/Player/Scripts/DarkPlayerController.cs
+++ Assets/Player/Scripts/DarkPlayerController.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class DarkPlayerController : MonoBehaviour {
 	public Vector2 targetVelocity;
 	private Animator animator;
+	public GameObject Bomb;
+	public int BombNum = 1;
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator>();
@@ -13,10 +15,23 @@ public class DarkPlayerController : MonoBehaviour {
 		rigidbody2D.velocity=targetVelocity * 2.5f;
 		animator.SetFloat("SpeedX",targetVelocity.x);
 		animator.SetFloat("SpeedY",targetVelocity.y);
+		if(Input.GetButtonDown("Fire1") && BombNum >0)
+		{
+			BombNum--;
+			PlaceBomb();
+
+		}
 	}
 	void LateUpdate(){
 		if(Input.GetKeyDown(KeyCode.Escape)){
 			Application.Quit();
 		}
+	}
+
+	public void PlaceBomb()
+	{
+		GameObject SetBomb = Instantiate(Bomb,transform.position,Quaternion.identity) as GameObject;
+
+		SetBomb.GetComponent<Bomb>().Player = gameObject;
 	}
 }
